2c94812e4b6e181f778b3fcd04f6988af3411fa0
luizeduuardo <luizeduuardo@gmail.com>
2025-07-19 12:53:19 -0300

    teste commit

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -12,7 +12,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwaggerUi(options =>
+    app.UseSwagger()
+    .UseSwaggerUi(options =>
     {
         options.DocumentPath = "/openapi/v1.json";
     });
